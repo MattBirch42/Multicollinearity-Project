a5af153792e5b010f01660694eaf3a95d6a86b09
Matt Birch <matt---@hotmail.com>
2022-07-21 10:55:54 -0700

    Built App. Draft 1 is complete. Not sure if I'll do more. This was a little project.

--- Multicollinearity_Demo_App/app.R
+++ Multicollinearity_Demo_App/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 #raw code copied from r file. Mostly putting this here to try committing document from R to Github. 
@@ -20,8 +11,8 @@ library(ggplot2)
 # There are nObs observations in each data set
 # the app will have nSets numbers of datasets with different 
 #          degrees of multicollinearity
-nObs <- 10
-nSets <- 4
+nObs <- 100
+nSets <- 10
 
 # random draws for features (xs) and targets (y)
 # column 3 (targets): y = x1+2x2+e
@@ -29,7 +20,7 @@ nSets <- 4
 set.seed(42)
 features_raw <- matrix(runif(2*nObs, -10,10), nrow = nObs)
 set.seed(11235)
-target_raw <- x_raw[,1]+2*x_raw[,2]+matrix(runif(nObs,-2,2))
+target_raw <- features_raw[,1]+2*features_raw[,2]+matrix(runif(nObs,-2,2))
 
 # Now we make the nSets of data based on these draws.
 # The first matrix has no multicollinearity and is the one we already drew
@@ -52,59 +43,60 @@ head(data_sets)
 # In summary, the same feature information is increasingly overlapped, but the target never changes. 
 
 #Now lets move on. Each of the nSets data sets will have a 3D plot,
-# squared sum of errors plotted on a range of beta1 and beta2 values.
+# squared sum of errors (SSE) plotted on a range of beta1 and beta2 values.
+#SSE is the sum of the squared values of the errors/noise terms, (y-yhat)
+
 num_betas <- 20
-beta1 <- seq(-20,20,length.out = num_betas)
-beta2 <- seq(-20,20,length.out = num_betas)
+beta1 <- seq(-50,50,length.out = num_betas)
+beta2 <- seq(-50,50,length.out = num_betas)
 sse_array <- array(0, dim = c(num_betas,num_betas,nSets))
 for (k in 1:nSets) {
   for (i in 1:num_betas) {
     for (j in 1:num_betas) {
       betas <- rbind(beta1[i],beta2[j])
       #calculating SSE for every beta pair in each matrix
-      sse_array[i,j,k] <- sum((target_raw[]-data_sets[,1:2,k]%*%betas)^2)
+      #Disclaimer: technically, this calculates the negative of the SSE.
+      #For visualization, it is easier for me to display maximizing a negative than
+      #      to depict minimizing. So the high points on these graphs are where 
+      #      SSE is minimized. It is mathematically equivalent.  
+      sse_array[i,j,k] <- -sum((target_raw[]-data_sets[,1:2,k]%*%betas)^2)
     }
   }
 }
-
-
-persp3D(beta1,beta2,sse_array[,,1],
-        phi = 10,
-        theta = 35,
-        label = TRUE,
-        xlab = "Beta 1",
-        ylab = "Beta 2",
-        zlab = "SSE",
-        scale = TRUE,
-        axes = TRUE,
-        ticktype = "detailed")
-
-
-
-
-
-
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Multicollinearity Visualization App"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
+      sliderInput("mcSet",
+                  "Multicollinearity Factor: Higher numbers for more multicollinearity:",
                   min = 1,
-                  max = 50,
-                  value = 30)
+                  max = nSets,
+                  value = 1),
+      sliderInput("userPhi",
+                   "Vertical Display Angle:",
+                   min = 0,
+                   max =  90,
+                   step = 5,
+                   value = 20),
+      sliderInput("userTheta",
+                  "Horizontal Display Angle:",
+                  min = 0,
+                  max =  180,
+                  step = 5,
+                  value = 20)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("ssePlot"),
+      textOutput("explanation")
     )
   )
 )
@@ -112,14 +104,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlot({
+  output$ssePlot <- renderPlot({
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    persp3D(beta1,beta2,sse_array[,,input$mcSet],
+            phi = input$userPhi,
+            theta = input$userTheta,
+            label = TRUE,
+            xlab = "Beta 1",
+            ylab = "Beta 2",
+            zlab = "SSE",
+            scale = TRUE,
+            axes = TRUE,
+            ticktype = "simple",
+            resfac = 3,
+            contour = TRUE,
+            main = "Sum of Squared Errors (-) for Bivariate Regression",
+            las = 0,
+            cex.lab = 1.5,
+            cex.main = 2,
+            d=3)
   })
+  output$explanation <- renderText("This graph shows the negative sum of squared errors (SSE) for many different combinations of beta1 and beta2 in a bivariate regression framework. This is easily generalized to higher dimensional regressions. When you run a regression, it chooses the betas to minimize the SSE. In reality, this is a minimization problem and the graph should look like a hole, but I graphed the negative of the SSE for visualization purposes. In this case, the highest (reddest) point/s on the graph show where SSE is minimized, and the associated beta1 and beta2 would be the optimized betas from the regression. Note that when whe have datasets with more multicollinearity (slide that 'Multicollinearity Factor' to the right), the set of red points grows to where there is no unique solution. That is why we have problems with multicollinearity. It makes it so regular regression cannot identify unique beta values, because multiple solutions exist. I also made it so you can fiddle around with the viewing angles. The code for this App can be found here: https://github.com/MattBirch42/Multicollinearity-Project.git")
 }
 
 # Run the application 
